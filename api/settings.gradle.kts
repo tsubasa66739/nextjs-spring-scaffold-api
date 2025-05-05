ec3b4e13e20548fb8f3427030482e47c092ed029
Tsubasa <tsubasa66739@gmail.com>
2025-05-05 17:37:15 +0900

    add api.

---
+++ api/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "nextjs-spring-scaffold-api"
---
+++ api/src/main/java/com/example/spring_boot_trial/SpringBootTrialApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_trial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTrialApplication.class, args);
+    }
+
+}
